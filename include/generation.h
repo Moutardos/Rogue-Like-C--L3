5882c237d7430326e4705ee1c7da47901c7f6f86
Moutard <loic.cozdenmat@gmail.com>
2021-10-31 17:56:12 +0100

    testpourvoirsicamarche

---
+++ include/generation.h
@@ -0,0 +1,8 @@
+#ifndef GENERATION
+#define GENERATION
+
+#define terrainW 43
+#define terrainH 63
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "test.h"
+
+int int main(int argc, char const *argv[])
+{
+	return 0;
+}
