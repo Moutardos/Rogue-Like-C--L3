dd4c879f0617cb3532d90bc143e0d92515cf7d56
Moutardos <loic.cozdenmat@gmail.com>
2022-01-12 16:39:05 +0100

    modification de monstre_fight pour respecter le sujet

--- src/monstre.c
+++ src/monstre.c
@@ -6,7 +6,7 @@ Monstre init_monstre(TypeMonstre type, int dif){
 	switch(type){
 		case ALIEN:
 			new_monstre.hp = 100;
-			new_monstre.atk = 2;
+			new_monstre.atk = 6;
 			new_monstre.miss = 1;
 			new_monstre.acc = 100;
 			new_monstre.vitesse = 1;
@@ -16,14 +16,27 @@ Monstre init_monstre(TypeMonstre type, int dif){
 }
 
 void monstre_fight(Monstre* monstre, Attribut* joueur_stat){
-	int roll = rand() % 100;
-
+	int roll = rand() % 101;
+	int damage;
+	float percent;
+	
+	/* AGGRO */
 	if (monstre->fight == 1){
+		
+		/* HIT */
 		if (roll > monstre->miss){
-			joueur_stat->Hp -= monstre->atk;
+			percent = (120 - (rand() % 40))/(100 * 1.0);
+						printf("percent = %f  ",percent);
+			damage = (int) (monstre->atk * (float)percent );
+			printf("damgage= %d  ",(int) (monstre->atk*(float)percent));
+			/* CRIT */
+			if(roll >= 95)
+				damage = damage *3;
+			
+			joueur_stat->Hp -= damage;
 			printf("hit ! joueur hp :%d\n", joueur_stat->Hp );
 		}
-	
+
 					printf(" miss    ");
 	}
 	else {
